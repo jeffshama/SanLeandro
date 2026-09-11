a42dc36412b3636e8c5fb30a58105c93a62a43b5
jeffshama <65435006+jeffshama@users.noreply.github.com>
2026-09-11 16:55:23 -0400

    New net8 and sql release

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,42 @@
-const CACHE_NAME = 'sanleandro-cache-20260711063809';
-const URLS_TO_CACHE = [
-  'index.html?rel=20260711063809',
-  'manifest.json?rel=20260711063809',
-  'sanleandro-icon.png'
+const CACHE_NAME = "san-leandro-20260911164318";
+
+const FILES_TO_CACHE = [
+    "index.html?rel=20260911164318",
+    "manifest.json?rel=20260911164318",
+    "sanleandro-icon.png"
 ];
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
 });
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.map(function(key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames =>
+                Promise.all(
+                    cacheNames
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
+                )
+            )
+            .then(() => self.clients.claim())
+    );
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+            })
+    );
 });
