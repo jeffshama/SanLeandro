46a923871ba68cad3deed6df9903fffef40cabf1
jeffshama <65435006+jeffshama@users.noreply.github.com>
2026-09-13 21:27:50 -0400

    new url preview

---
+++ sw.js
@@ -0,0 +1,37 @@
+
+const CACHE_NAME = 'stoner-directory-20260913211736';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './images/splash.png',
+  './images/og-preview.png',
+  './images/silhouette.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
