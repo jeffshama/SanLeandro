e5afd5814031eb211c2d6a941075098015a5fc49
jeffshama <65435006+jeffshama@users.noreply.github.com>
2026-09-20 14:24:00 -0400

    September Update

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,41 @@
-const CACHE_NAME = "san-leandro-20260918123948";
-
-const FILES_TO_CACHE = [
-    "index.html?rel=20260918123948",
-    "manifest.json?rel=20260918123948",
-    "sanleandro-icon.png"
+var cacheName = 'sanleandro-09 20 26 1418';
+var indexUrl = 'index.html?rel=09%2020%2026%201418';
+var cacheUrls = [
+  indexUrl,
+  'manifest.json?rel=09%2020%2026%201418',
+  'sanleandro-icon.png'
 ];
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
-    );
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function (cache) { return cache.addAll(cacheUrls); })
+      .then(function () { return self.skipWaiting(); })
+  );
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys()
-            .then(cacheNames =>
-                Promise.all(
-                    cacheNames
-                        .filter(cacheName => cacheName !== CACHE_NAME)
-                        .map(cacheName => caches.delete(cacheName))
-                )
-            )
-            .then(() => self.clients.claim())
-    );
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (names) {
+        return Promise.all(names.filter(function (name) { return name !== cacheName; })
+                                .map(function (name) { return caches.delete(name); }));
+      })
+      .then(function () { return self.clients.claim(); })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') { return; }
+  event.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request, { ignoreSearch: true }).then(function (cached) {
+        if (cached) { return cached; }
+        return fetch(event.request).catch(function () {
+          if (event.request.mode === 'navigate') { return cache.match(indexUrl); }
+          return Response.error();
+        });
+      });
+    })
+  );
 });
